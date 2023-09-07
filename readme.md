79bee93783992c140556e03e48592bbe235fe4c4
sik2 <devpang20@gmail.com>
2023-09-07 21:49:02 +0900

    onPressNumber, onPressOperation 벨리데이션

--- readme.md
+++ readme.md
@@ -6,7 +6,8 @@
 - [x] calculator class 만들기
 - [x] 계산기 기능 관련 DOM 셋팅
 - [x] 숫자 버튼 기능(onPressNumber) 구현
-- [ ] 사칙연산 버튼 기능 구현
-- [ ] 연산 기능 구현
+- [x] 사칙연산 버튼 기능(onPressOperation) 구현
+- [ ] onPressNumber, onPressOperation 벨리데이션
+- [ ] onEqual 기능 구현
 - [ ] onReset 기능 구현
 - [ ] onDelete 기능 구현
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 class Calculator {
     $previousPreview
     $currentPreview
+    // 연산 관련 변수 추가
 
     constructor($previousPreview, $currentPreview) {
         this.$previousPreview = $previousPreview
@@ -8,11 +9,16 @@ class Calculator {
     }
 
     onPressNumber(number) {
-        console.log(this.$previousPreview.textContent)
-        console.log(this.$currentPreview.textContent)
         // TODO: 벨리데이션 처리 
         // if () {}
         this.$currentPreview.textContent += number
+    }
+
+    onPressOperation(operation) {
+        // TODO: 벨리데이션 처리 
+        // if () {}
+        this.$previousPreview.textContent = `${this.$currentPreview.textContent} ${operation}`
+        this.$currentPreview.textContent = ''
     }
 }
 
@@ -25,6 +31,7 @@ const $minus = document.querySelector("[data-btn-minus]")
 const $plus = document.querySelector("[data-btn-plus]")
 const $multiply = document.querySelector("[data-btn-multiply]")
 const $divide = document.querySelector("[data-btn-divide]")
+const $equal = document.querySelector("[data-btn-equal]")
 
 // 숫자 or 연산
 const $numbers = document.querySelectorAll("[data-btn-number]")
@@ -44,7 +51,25 @@ $numbers.forEach((number) => {
 
 $operations.forEach((operation) => {
     operation.addEventListener("click", (e) => {
-        console.log(e.target.textContent)
+        switch (operation) {
+            case $plus:
+             calculator.onPressOperation("+") 
+             break
+            case $minus:
+             calculator.onPressOperation("-") 
+             break
+            case $multiply: 
+             calculator.onPressOperation("×")
+             break
+            case $divide: 
+             calculator.onPressOperation("÷")
+             break
+            case $equal: 
+             calculator.onPressOperation()
+             break
+            default: 
+             break
+        }
     })
     
 })
