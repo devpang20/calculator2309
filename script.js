d1760815db85c471e7be4a24d43e991c9220a077
sik2 <devpang20@gmail.com>
2023-09-07 21:10:20 +0900

    DOM 셋팅

--- script.js
+++ script.js
@@ -2,10 +2,35 @@ class Calculator {
 
 }
 
+// 값 표시
 const $previoutPreview = document.querySelector("[data-previous-preview]")
 const $currentPreview = document.querySelector("[data-currnet-preview]")
 
-console.log($previoutPreview)
-console.log($currentPreview)
+// 사칙연산
+const $minus = document.querySelector("[data-btn-minus]")
+const $plus = document.querySelector("[data-btn-plus]")
+const $multiply = document.querySelector("[data-btn-multiply]")
+const $divide = document.querySelector("[data-btn-divide]")
+
+// 숫자 or 연산
+const $numbers = document.querySelectorAll("[data-btn-number]")
+const $operations = document.querySelectorAll("[data-btn-operation]")
+
+console.log($numbers)
+console.log($operations)
+
+$numbers.forEach((number) => {
+    number.addEventListener("click", (e) => {
+        console.log(e.target.textContent)
+    })
+    
+})
+
+$operations.forEach((operation) => {
+    operation.addEventListener("click", (e) => {
+        console.log(e.target.textContent)
+    })
+    
+})
 
 const calculator = new Calculator()
