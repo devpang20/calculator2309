7c430962c5351a71e1136bd21dc14a46ea5cbd89
sik2 <devpang20@gmail.com>
2023-09-07 21:25:36 +0900

    숫자 버튼 기능(onPressNumber) 구현

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 class Calculator {
+    $previousPreview
+    $currentPreview
 
+    constructor($previousPreview, $currentPreview) {
+        this.$previousPreview = $previousPreview
+        this.$currentPreview = $currentPreview
+    }
+
+    onPressNumber(number) {
+        console.log(this.$previousPreview.textContent)
+        console.log(this.$currentPreview.textContent)
+        // TODO: 벨리데이션 처리 
+        // if () {}
+        this.$currentPreview.textContent += number
+    }
 }
 
 // 값 표시
-const $previoutPreview = document.querySelector("[data-previous-preview]")
+const $previousPreview = document.querySelector("[data-previous-preview]")
 const $currentPreview = document.querySelector("[data-currnet-preview]")
 
 // 사칙연산
@@ -16,12 +30,14 @@ const $divide = document.querySelector("[data-btn-divide]")
 const $numbers = document.querySelectorAll("[data-btn-number]")
 const $operations = document.querySelectorAll("[data-btn-operation]")
 
-console.log($numbers)
-console.log($operations)
 
+const calculator = new Calculator($previousPreview, $currentPreview)
+
+// 숫자 처리
 $numbers.forEach((number) => {
     number.addEventListener("click", (e) => {
-        console.log(e.target.textContent)
+        const number = e.target.textContent
+        calculator.onPressNumber(number)
     })
     
 })
@@ -32,5 +48,3 @@ $operations.forEach((operation) => {
     })
     
 })
-
-const calculator = new Calculator()
